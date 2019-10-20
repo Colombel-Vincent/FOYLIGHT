7cd02035a8053f1e8df3d9a62c6338c26fdd147d
Colombel-Vincent <colombel.vincent.27@gmail.com>
2019-10-20 19:16:20 +0200

    add files effects

---
+++ src/Effects.hpp
@@ -0,0 +1,36 @@
+#ifndef __EFFECTS_HPP__
+#define __EFFECTS_HPP__
+
+// ─────────────────────────────────────────────────────────────
+//					INCLUDE
+// ─────────────────────────────────────────────────────────────
+
+// C Header
+
+// C++ Header
+#include <QObject>
+#include "FOYLIGHT.hpp"
+#include "Fixture.hpp"
+// Dependencies Header
+#include <QQmlPtrPropertyHelpers.h>
+#include <QQmlAutoPropertyHelpers.h>
+
+
+// Application Header
+
+FOYLIGHT_NAMESPACE_START
+// ─────────────────────────────────────────────────────────────
+//					DECLARATION
+// ─────────────────────────────────────────────────────────────
+
+
+// ─────────────────────────────────────────────────────────────
+//					FUNCTION
+// ─────────────────────────────────────────────────────────────
+
+
+
+};
+FOYLIGHT_NAMESPACE_END
+
+#endif
